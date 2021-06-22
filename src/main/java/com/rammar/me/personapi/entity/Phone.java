dc334cbc178a6c087c1f2829ccb826f6487cd763
rammaR <rtawera@gmail.com>
2021-06-22 14:47:42 -0300

    ADiciona integração JPA

---
+++ src/main/java/com/rammar/me/personapi/entity/Phone.java
@@ -0,0 +1,29 @@
+package com.rammar.me.personapi.entity;
+
+import com.rammar.me.personapi.enums.PhoneType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Phone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private PhoneType type;
+
+    @Column(nullable = false)//campo obrigatório
+    private String number;
+
+}
