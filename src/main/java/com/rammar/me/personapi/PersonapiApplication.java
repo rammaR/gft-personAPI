bbf2479bef48fd2ebb4ef68117fc02623749ae52
Ramar <rtawera@gmail.com>
2021-07-06 09:38:07 -0300

    Ignore o application.yml

--- src/main/java/com/rammar/me/personapi/PersonapiApplication.java
+++ src/main/java/com/rammar/me/personapi/PersonapiApplication.java
@@ -1,18 +1,7 @@
 package com.rammar.me.personapi;
 
-import com.rammar.me.personapi.controller.PersonController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class PersonapiApplication {
